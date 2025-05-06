13d98fdd864553b7be36e1da99aa97b1b78bd80e
Jajar Sumbogo <119916348+Nanno97@users.noreply.github.com>
2025-05-06 13:05:38 +0700

    API

--- kostfin/admin/views.py
+++ kostfin/admin/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render;
+from rest_framework import viewsets;
+from .models import Admin;
+from .serializers import AdminSerializer;
 # Create your views here.
+
+class AdminViewSet(viewsets.ModelViewSet):
+    queryset = Admin.objects.all();
+    serializer_class = AdminSerializer;
--- kostfin/pencari/views.py
+++ kostfin/pencari/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets;
+from .models import *;
+from .serializers import *;
 # Create your views here.
+
+class PenghuniKostViewSet(viewsets.ModelViewSet):
+    queryset = PenghuniKost.objects.all();
+    serializer_class = PenghuniKostSerializer;
+
+class NotifikasiViewSet(viewsets.ModelViewSet):
+    queryset = Notifikasi.objects.all();
+    serializer_class = NotifikasiSerializer;
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all();
+    serializer_class = BookingSerializer;
+
+class PembayaranViewSet(viewsets.ModelViewSet):
+    queryset = Pembayaran.objects.all();
+    serializer_class = Pembayaranserializers;
+
+class ReviewRatingViewSet(viewsets.ModelViewSet):
+    queryset = ReviewRating.objects.all();
+    serializer_class = ReviewRatingSerializer;
